863b0b6220074cbe15114405b1da6445f99503cf
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-09-22 22:04:47 -0400

    added metadata, fontawesome icons in header, changed link look/feel

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,13 +12,13 @@ interface Props {
 const Header: React.FC<Props> = ({ icon, name, tagline, contact }) => (
     <header className="header">
         <h1><span className="no-print"><i className={icon}></i></span>{name}</h1>
-        <p>{tagline}</p>
+        <p className="tagline">{tagline}</p>
         <ul>
-            <li>{contact.location}</li>
-            <li><a href={`mailto:${contact.email}`}>{contact.email}</a></li>
-            <li>{contact.phone}</li>
-            <li><a href={contact.website}>Website</a></li>
-            <li><a href={contact.github}>GitHub</a></li>
+            <li><i className="fas fa-map-marker-alt"></i>{contact.location}</li>
+            <li><i className="fas fa-envelope"></i><a href={`mailto:${contact.email}`}>{contact.email}</a></li>
+            <li><i className="fas fa-phone"></i><a href={`tel:+1${contact.phone.replace(/[\s()-]/g, "")}`}>{contact.phone}</a></li>
+            <li><i className="fas fa-globe"></i><a href={contact.website}>{contact.website.substring(8)}</a></li>
+            <li><i className="fab fa-github"></i><a href={contact.github}>{contact.github.substring(8)}</a></li>
         </ul>
     </header>
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Sidebar from '../components/Sidebar';
 // import Section from '../components/Section';
 // import Experience from '../components/Experience';
 // import Projects from '../components/Projects';
+import Head from 'next/head';
 import '../styles/main.scss';
 
 export default function Home() {
@@ -19,9 +20,38 @@ export default function Home() {
     if (!resume) return <div>Loading...</div>;
 
     return (
-        <div className="left-container">
-            <Header icon={resume.icon} name={resume.name} tagline={resume.tagline} contact={resume.contact} />
-            <Sidebar skills={resume.skills} education={resume.education} />
+        <>
+        <Head>
+            <title>{resume.name} - Resume</title>
+            <meta name="description" content={resume.tagline} />
+            <meta property="og:title" content={`${resume.name} - Resume`} />
+            <meta property="og:type" content="WebPage" />
+            <link
+                rel="stylesheet" 
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+                crossOrigin='anonymous'
+                referrerPolicy='no-referrer'
+            />
+            <script
+                type="application/ld+json"
+                    dangerouslySetInnerHTML={{
+                        __html: JSON.stringify({
+                            "@context": "https://schema.org",
+                            "@type": "WebPage",
+                            "name": "Christopher Ruckman Resume",
+                            "description": "Multidisciplinary engineer, author, and legal advocate"
+                        })
+                    }}
+            />
+        </Head> 
+
+        <div className='container'>
+            <div className="sidebar-container">
+                <Header icon={resume.icon} name={resume.name} tagline={resume.tagline} contact={resume.contact} />
+                <Sidebar skills={resume.skills} education={resume.education} />
+            </div>
+            <div className="content-container">LA LA LA</div>
         </div>
+        </>
     )
 }
