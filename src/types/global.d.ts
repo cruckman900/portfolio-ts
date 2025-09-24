31c0e42302fe0da43fc9927bffe2d7a722afe62a
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-09-24 18:38:51 -0400

    created conn for mongoose

---
+++ src/types/global.d.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+declare global {
+    var mongooseConnection: {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+    };
+}
