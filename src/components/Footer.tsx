78e796455b2ad8eef7cc153d7e95af2374842a25
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-09-25 05:38:29 -0400

    added critters

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,12 +5,12 @@ const Footer: React.FC = () => {
     return (
         <footer className='footer'>
             <div className="main">
-                <p>&copy;{new Date().getFullYear()} Christopher Ruckman</p>
                 <div className="social-links">
                     <a href="https://github.com/cruckman">GitHub</a>
                     &nbsp; | &nbsp;
                     <a href="/contact">Contact</a>
                 </div>
+                <p>&copy;{new Date().getFullYear()} Christopher Ruckman</p>
             </div>
             <div className="print">
                 <button className="print-button" onClick={() => window.print()}>Print Page</button>
