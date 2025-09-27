73837335d1837ae240d0ca366413648e7e985a58
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-09-27 02:01:57 -0400

    cleaned up resume. got rid of unnecessarycomponents

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,20 +1,51 @@
 import Head from 'next/head';
 import TwoPanelLayout from '@/components/layout/TwoPanelLayout';
 import Layout from "@/components/layout/Layout";
-import ResumeHeader from '@/components/Page/Resume/ResumeHeader';
 import Section from '@/components/ui/Section';
-import Achievements from '@/components/Page/Resume/Achievements';
-import Experience from '@/components/Page/Resume/Experience';
-import ProjectList from '@/components/Page/Resume/ProjectList';
 import resumeData from '@/data/resume.json';
+import styles from './resume.module.scss';
+import '@/styles/page/resume.scss';
 export default function ResumePage() {
     const leftPanel = (
-        <ResumeHeader name={resumeData.name} tagline={resumeData.tagline} contact={resumeData.contact} />
-        // <Sidebar
-        //     skills={resumeData.skills}
-        //     education={resumeData.education}
-        // >
-        // </Sidebar>
+        <>
+            <header className="resume-header">
+                <h1>{resumeData.name}</h1>
+                <p className="tagline">{resumeData.tagline}</p>
+                <ul>
+                    <li><i className="fas fa-map-marker-alt"></i>{resumeData.contact.location}</li>
+                    <li><i className="fas fa-envelope"></i><a href={`mailto:${resumeData.contact.email}`}>{resumeData.contact.email}</a></li>
+                    <li><i className="fas fa-phone"></i><a href={`tel:+1${resumeData.contact.phone.replace(/[\s()-]/g, "")}`}>{resumeData.contact.phone}</a></li>
+                    <li><i className="fas fa-globe"></i><a href={resumeData.contact.website} target='_blank'>{resumeData.contact.website.substring(8)}</a></li>
+                    <li><i className="fab fa-github"></i><a href={resumeData.contact.github} target='_blank'>{resumeData.contact.github.substring(8)}</a></li>
+                </ul>
+            </header>
+            <Section title="Core Competencies" icon="fas fa-key">
+                <>
+                    {resumeData.skills.map((skill, i) =>
+                    (
+                        <div className='sidebar-content' key={i}>
+                            <h3>{skill.section}</h3>
+                            <div>{skill.content}</div>
+                        </div>
+                    )
+                    )}
+                </>
+            </Section>
+
+            <Section className={styles.pagebreakBefore} title="Education" icon="fas fa-graduation-cap">
+                <>
+                    {resumeData.education.map((edu, i) =>
+                    (
+                        <div className='sidebar-content' key={i}>
+                            <div><i>{edu.dates}</i></div>
+                            <div>{edu.degree}</div>
+                            <h3><a href={edu.url} target='_blank'>{edu.school}</a></h3>
+                        </div>
+                    )
+                    )}
+                </>
+            </Section>
+        </>
     );
 
     const rightPanel = (
@@ -23,13 +54,59 @@ export default function ResumePage() {
                 <p>{resumeData.summary}</p>
             </Section>
             <Section title="Selected Achievements" icon="fas fa-star">
-                <Achievements achievements={resumeData.achievements} />
+                <>
+                    {resumeData.achievements.map((ach, i) =>
+                    (
+                        <div key={i}>
+                            <h3>{ach.category}</h3>
+                            <ul>
+                                {ach.items.map((item, j) => (
+                                    <li key={j}>{item}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )
+                    )}
+                </>
             </Section>
-            <Section title="Professional Experience" icon="fas fa-briefcase">
-                <Experience experience={resumeData.experience} />
+            <Section className={styles.pagebreakBefore} title="Professional Experience" icon="fas fa-briefcase">
+                <>
+                    {resumeData.experience.map((exp, i) =>
+                    (
+                        <article className={styles.noBreakInside} key={i}>
+                            <h3>{exp.title} &mdash; {exp.company}</h3>
+                            <div><i>{exp.dates}</i></div>
+                            <ul>
+                                {exp.details.map((item, j) => (
+                                    <li key={j}>{item}</li>
+                                ))}
+                            </ul>
+                        </article>
+                    )
+                    )}
+                </>
             </Section>
             <Section title="Personal Projects" icon="fas fa-code">
-                <ProjectList projectsList={resumeData.projects} />
+                <>
+                    {resumeData.projects.map((list, i) =>
+                    (
+                        <article className={styles.noBreakInside} key={i}>
+                            <h3>{list.genre}</h3>
+                            {list.projects.map((project, j) =>
+                            (
+                                <div key={j}>
+                                    <i>{project.name}</i>
+                                    <ul>
+                                        <li>{project.description}</li>
+                                        <li><strong>Tech: </strong>{project.tech}</li>
+                                    </ul>
+                                </div>
+                            )
+                            )}
+                        </article>
+                    )
+                    )}
+                </>
             </Section>
         </main>
     );
