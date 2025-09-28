9e6e435a950cfe2012a0cd938188e57409621455
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-09-28 13:01:29 -0400

    finalizing SEO stoofs

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -21,27 +21,28 @@ const Layout: React.FC<Props> = ({ children }) => {
 
     return (
         <>
-            <Head key="Home">
+            <Head>
                 <title>LinearDescent | Poetic Survival Through Code, Art & Memoir</title>
-                <meta name="description" content="Poetic survival platform from Rayland, Ohio—memoirs, glitch art, ambient guitar, and expressive full-stack engineering."></meta>
-                <link rel="canonical" href="https://lineardescent.com" />
+                <meta name="description" content="Poetic survival platform from Rayland, Ohio—memoirs, glitch art, ambient guitar, and expressive full-stack engineering." />
+                <link rel="canonical" href="https://lineardescent.netlify.app" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+                {/* Favicon */}
+                <link rel="icon" type="image/png" href="/favicon.png" />
 
                 {/* Open Graph */}
                 <meta property="og:title" content="LinearDescent | Poetic Survival Through Code, Art & Memoir" />
                 <meta property="og:description" content="Memoirs, glitch art, ambient guitar, and expressive full-stack engineering from Rayland, Ohio." />
-                <meta property="og:url" content="https://lineardescent.com" />
+                <meta property="og:url" content="https://lineardescent.netlify.app" />
                 <meta property="og:type" content="website" />
-                {/* <meta property="og:image" content="https://lineardescent.com/og-image.jpg" /> */}
-                <meta property="og:image" content="https://lineardescent.netlify.app/og-image.jpg" />
+                <meta property="og:image" content="https://lineardescent.netlify.app/og-image.png" />
 
                 {/* Twitter */}
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content="LinearDescent | Poetic Survival Through Code, Art & Memoir" />
                 <meta name="twitter:description" content="Memoirs, glitch art, ambient guitar, and expressive full-stack engineering from Rayland, Ohio." />
-                {/* <meta name="twitter:image" content="https://lineardescent.com/og-image.jpg" /> */}
-                <meta property="og:image" content="https://lineardescent.netlify.app/og-image.jpg" />
+                <meta name="twitter:image" content="https://lineardescent.netlify.app/og-image.png" />
             </Head>
-
             <div className='container'>
                 <header className='header'><Navbar className="navbar" /></header>
                 <main className='content'>{children}</main>
