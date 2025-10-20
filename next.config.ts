f5f8f3687237d1342165779d6c3525a98b035774
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-10-20 19:26:52 -0400

    don't run next dev

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,23 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   images: {
     domains: ['your-image-domains.com'], // optional
   },
-  webpack(config: Configuration) {
+  webpack(config: Configuration, { isServer }) {
+    // Enable source maps for server-side
+    if (isServer) {
+      config.devtool = 'source-map'
+    }
+
+    // Set publicPath
     config.output = {
       ...config.output,
       publicPath: '/',
     }
 
+    // Add SVG loader
     config.module?.rules?.push({
       test: /\.svg$/,
       issuer: /\.[jt]sx?$/,
