261b771a8c72e7288a4b3023e1ec6f473db3c420
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2025-12-06 22:34:16 -0500

    applied react2shell fix

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -8,32 +8,25 @@ interface ThemeContextType {
 
 const ThemeContext = createContext<ThemeContextType>({
     theme: 'light',
-    setTheme: () => {},
+    setTheme: () => { },
 })
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
     const [theme, setThemeState] = useState('light')
 
     const updateTheme = (val: string) => {
-        // console.log('ThemeContext.tsx -- Setting theme to: before setting state', val);
         setThemeState(val)
         localStorage.setItem('theme', val)
         document.documentElement.setAttribute('data-theme', val)
         window.dispatchEvent(new Event('themeChange'))
-        // console.log('ThemeContext.tsx -- after etting attribute data-theme to: before setting state', val);
     }
 
     useEffect(() => {
         const stored = localStorage.getItem('theme')
         if (stored) {
-            // console.log('ThemeContext.tsx -- useEffect getItem from localStorage', stored)
             updateTheme(stored)
         }
     }, [])
-
-    // useEffect(() => {
-    //     console.log('ThemeContext.tsx -- theme state changed to', theme);
-    // })
 
     return (
         <ThemeContext.Provider value={{ theme, setTheme: updateTheme }}>
