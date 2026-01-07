4b309fb9099722a544a1dd97e8610466d31e9e93
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2026-01-06 20:23:03 -0500

    background for footer changed

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,16 +4,18 @@ import styles from './Footer.module.scss'
 
 const Footer: React.FC = () => {
     return (
-        <footer className={styles.footer}>
-            <div>
-                <div className={styles.socialLinks}>
-                    <Link className={styles.link} href="https://github.com/cruckman"><i className="fa-brands fa-github"></i>GitHub</Link>
-                    &nbsp; | &nbsp;
-                    <Link className={styles.link} href="/contact"><i className="fa-solid fa-envelope"></i>Contact</Link>
+        <div className={styles.container}>
+            <footer className={styles.footer}>
+                <div>
+                    <div className={styles.socialLinks}>
+                        <Link className={styles.link} href="https://github.com/cruckman"><i className="fa-brands fa-github"></i>GitHub</Link>
+                        &nbsp; | &nbsp;
+                        <Link className={styles.link} href="/contact"><i className="fa-solid fa-envelope"></i>Contact</Link>
+                    </div>
+                    <div className={styles.footerText}><i className="fa-solid fa-copyright"></i>{new Date().getFullYear()} LinearDescent</div>
                 </div>
-                <div className={styles.footerText}><i className="fa-solid fa-copyright"></i>{new Date().getFullYear()} LinearDescent</div>
-            </div>
-        </footer>
+            </footer>
+        </div>
     )
 }
 
